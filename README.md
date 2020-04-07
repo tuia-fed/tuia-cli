e32bbfb26f3b9c204fc263af7bdf4b0973fac6c8
minjs.cn <zm@tuia.cn>
2020-04-07 16:17:19 +0800

    first-commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# tuia-cli
+
+> tuia-cli 用于创建活动工程项目
+
+## Usage
+
+```
+npm install global @tuia/cli
+```
---
+++ bin/main.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const { program } = require('commander')
+const pkg = require('../package.json')
+
+// 基本设置
+program
+  .version('tuia-cli ' + pkg.version)
+  .usage('<command> [options]')
+
+// 创建项目
+program
+  .command('create <app-name>')
+  .description('Create a new project powered by tuia-cli')
+  .action((name, cmd) => {
+    require('../lib/create')(name, cleanArgs(cmd))
+  })
+
+// 执行参数解析
+program
+  .parse(process.argv)
+
+
+function camelize (str) {
+  return str.replace(/-(\w)/g, (_, c) => c ? c.toUpperCase() : '')
+}
+
+function cleanArgs (cmd) {
+  const args = {}
+  cmd.options.forEach(o => {
+    const key = camelize(o.long.replace(/^--/, ''))
+    // if an option is not present and Command has a method with the same name
+    // it should not be copied
+    if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
+      args[key] = cmd[key]
+    }
+  })
+  return args
+}
